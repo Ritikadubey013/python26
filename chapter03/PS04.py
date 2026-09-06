12d1d005137f0faebe9a9af96afface921f27066
ritika dubey <ritikadubey1305@gmail.com>
2026-09-06 19:15:52 +0530

    commit

---
+++ chapter03/PS04.py
@@ -0,0 +1,7 @@
+name="Ritika is a   good   girl"
+
+print(name.replace("  "," "))
+print(name) 
+# string is immutable agr jo bhi replace kre hum us variable me 
+# but if we print that variable again it will be as it is 
+# thats why we call string as immutable 
