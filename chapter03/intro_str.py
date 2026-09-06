38c8ffdff9cf35e92c15b9acbd1d9ba21ef3b4f6
ritika dubey <ritikadubey1305@gmail.com>
2026-09-06 19:15:29 +0530

    commit

---
+++ chapter03/intro_str.py
@@ -0,0 +1,13 @@
+name="ritika"
+name2='ritika'
+name3='''ritika''' 
+
+print(name,name2,name3)
+
+# string is immutable exsisting string cant be changed 
+
+Name=name[0:5] # start from index 0 all way till 5 but 
+# exculing 5th index 
+print(Name)
+character=name[3]
+print(character)
