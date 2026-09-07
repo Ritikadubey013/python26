04ee5f163816c9b4c8e7b423dc18f1724515d421
ritika dubey <ritikadubey1305@gmail.com>
2026-09-07 20:23:22 +0530

    COMMIT

---
+++ gptPS/PS03.py
@@ -0,0 +1,10 @@
+a=int(input("enter marks in maths:"))
+b=int(input("enter marks in python:"))
+c=int(input("enter marks in DBMS:"))
+sum=a+b+c
+
+print("total marks out of 300 is :",sum)
+avg=sum/3
+print("average:",avg)
+
+print("percentage:",(sum/300)*100)
