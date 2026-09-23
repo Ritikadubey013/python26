ac25a5d2944b3a18d0ac33ee4b28785836b17397
ritika dubey <ritikadubey1305@gmail.com>
2026-09-23 20:32:50 +0530

    commit

--- chapter03/PS03.py
+++ chapter03/PS03.py
@@ -3,3 +3,4 @@ name="Ritika is a   good   girl"
 print(name.find("  "))
 # output me -1 means no double space if any other positive no then
 # yes double space is there 
+# how to check ki pure sentence me kaha kaha double space haii
