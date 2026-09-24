5022e5c3b3530d005919cca2822ba63cb91c63c6
ritika dubey <ritikadubey1305@gmail.com>
2026-09-24 19:05:53 +0530

    commit

---
+++ chapter04/list_01.py
@@ -0,0 +1,13 @@
+friends=["apple","orange",5,345.6,False,"jatin","ritika"]
+print (friends[0])
+friends[0]="grapes"
+print (friends[0:4])
+
+# list can store diff type of data in single variable 
+# cant change the exixting array by substituting diff value in its index
+# friend[0]=L CANT HAPPEN 
+# Strings are immutable
+# lists me we can mutate it 
+friends="apple"
+print (friends[0])
+print (friends[0])
