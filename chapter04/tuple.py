4a34bab0a8fe17cf4192966b6221bb1795135c6d
ritika dubey <ritikadubey1305@gmail.com>
2026-09-24 19:06:15 +0530

    commit

---
+++ chapter04/tuple.py
@@ -0,0 +1,16 @@
+a=(1,2,3,4,5,6)  
+# tuples are fixed values cant be changed 
+print(type(a))
+
+b=() #empty tuple
+
+c=(1)  # it will give type as int 
+d=(1,) # it will give type as tuple
+
+print(type(c))
+print(type(d))
+
+e=(1.34,56,88,"ritika","arya",False)
+print(type(e))
+
+# tuples cant be changed coz its immutable 
