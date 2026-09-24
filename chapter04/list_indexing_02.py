dd58ed7025e997b76419cbeb9714f9dcdebdd56b
ritika dubey <ritikadubey1305@gmail.com>
2026-09-24 19:06:00 +0530

    commit

---
+++ chapter04/list_indexing_02.py
@@ -0,0 +1,15 @@
+friends=["apple","orange",5,345.6,False,"jatin","ritika"]
+print(friends)
+
+friends.append("arya")
+print(friends)
+
+friends.insert(1,"sapna") # here at the index place of 1 sapna will get inserted or index+1
+print(friends)
+
+friends.reverse()
+print (friends)
+
+friends.pop(2)
+friends.remove("orange")
+print (friends)
